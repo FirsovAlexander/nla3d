5c250385623da53ee50b0eed69773a0a906193c5
SchetininVitaliy <sch_vitaliy@mail.ru>
2018-04-28 17:09:38 +0300

    INTER3 Jacobian inverting fixed

--- src/lib/elements/INTER3.cpp
+++ src/lib/elements/INTER3.cpp
@@ -31,7 +31,6 @@ void ElementINTER3::buildK() {
     math::Mat<3,18> matB = make_B(np);
     matBTDBprod(matB, D, dWt, Ke);
   }// loop over integration points
-
   assembleK(Ke, {Dof::UX, Dof::UY, Dof::UZ});
 }
 
@@ -43,11 +42,12 @@ void ElementINTER3::update () {
 void ElementINTER3::makeJacob(){
   //Находим координаты узлов в локальной декартовой системе координат треугольника (t1, t2)
   //Начало координт - первый узел треугольника
-  math::Vec<2> locX1(0,0);
+  math::Vec<2> locX1(0.,0.);
   //Вектор s1 направлен по одной из сторон
   math::Vec<3> t1 = storage->getNode(getNodeNumber(1)).pos-storage->getNode(getNodeNumber(0)).pos;
   math::Vec<3> t2 = storage->getNode(getNodeNumber(2)).pos-storage->getNode(getNodeNumber(0)).pos;
-  math::Vec<2> locX2(t1.length(),0);
+
+  math::Vec<2> locX2(t1.length(),0.);
   //Для координат третьего узла нужна ортогонолизация. Ищем угол треугольника при начале координат
   double mult = t1[0]*t2[0] + t1[1]*t2[1] + t1[2]*t2[2];
   double angle = acos(mult/t1.length()/t2.length());
@@ -55,7 +55,9 @@ void ElementINTER3::makeJacob(){
 
   math::Mat<2,2> J(locX1[0]-locX3[0],locX1[1]-locX3[1],
                    locX2[0]-locX3[0],locX2[1]-locX3[1]);
-  det = J.det(); //Якобиан перехода между L координатами и локальными декартовыми
+  math::Mat<2,2> invJ = J.inv(J.det());
+
+  det = abs(invJ.det()); //Якобиан перехода между L координатами и локальными декартовыми
 }
 
 void ElementINTER3::make_D(math::MatSym<3>& D){
@@ -102,6 +104,9 @@ math::Mat<3,3> ElementINTER3::make_T(){
   //Востанавливаем s2 как векторное произведение n x s1
   math::Vec<3> s2(n[1]*s1[2]-n[2]*s1[1],n[2]*s1[0]-n[0]*s1[2],n[0]*s1[1]-n[1]*s1[0]);
 
+  n = n*(1./n.length());
+  s1 = s1*(1./s1.length());
+  s2 = s2*(1./s2.length());
   //Матрица поворота от глоб к локальной ск
   math::Mat<3,3> T (s1[0],s2[0],n[0],
                     s1[1],s2[1],n[1],
